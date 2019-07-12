118d01c15d0f6b9c19a54c58a1c6943c07237297
Łukasz Opioła <lukasz.opiola@gmail.com>
2019-07-12 09:40:08 +0200

    VFS-5444 Change default parameters in init-letsencrypt.sh (onedata.org)

--- homepage/onedata.org/init-letsencrypt.sh
+++ homepage/onedata.org/init-letsencrypt.sh
@@ -11,8 +11,8 @@ fi
 domains=(onedata.org www.onedata.org)
 rsa_key_size=4096
 data_path="./data/certbot"
-email="ymopiola@cyfronet.pl"
-staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
+email="info@onedata.org"
+staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
