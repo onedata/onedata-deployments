efa8dd67c2a8ff7d201688cb91162ec815b4a5a6
Łukasz Opioła <ymopiola@cyfronet.pl>
2023-05-18 09:48:37 +0200

    Adjust the log in distribute-secret-config.sh

--- onezone/datahub.egi.eu/distribute-secret-config.sh
+++ onezone/datahub.egi.eu/distribute-secret-config.sh
@@ -8,7 +8,7 @@ MAILER_CONFIG_PATH="${PWD}/data/secret/mailer.config"
 EMERGENCY_PASSPHRASE_PATH="${PWD}/data/secret/emergency-passphrase.txt"
 
 for NODE in ${ALL_NODES}; do
-    echo "Copying auth.config and emergency-passphrase.txt to ${NODE}:"
+    echo "Copying auth.config, mailer.config and emergency-passphrase.txt to ${NODE}:"
     scp ${AUTH_CONFIG_PATH} ${NODE}:${AUTH_CONFIG_PATH}
     scp ${MAILER_CONFIG_PATH} ${NODE}:${MAILER_CONFIG_PATH}
     scp ${EMERGENCY_PASSPHRASE_PATH} ${NODE}:${EMERGENCY_PASSPHRASE_PATH}
