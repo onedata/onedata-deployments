dc45a9b5e82524806855fb91e04295fa3ceaa928
Łukasz Opioła <ymopiola@cyfronet.pl>
2024-03-06 16:57:29 +0100

    Add a step that prunes unused volumes after homepage update

--- homepage/onedata.org/update-homepage.py
+++ homepage/onedata.org/update-homepage.py
@@ -94,6 +94,9 @@ def deploy_static_files_from_docker(docker):
     print('Removing the docker image ({})...'.format(docker))
     cmd(['docker', 'rmi', docker])
 
+    print('Pruning unused docker volumes...')
+    cmd(['docker', 'volume', 'prune', '--force'])
+
     print('')
     print('All done! Visit onedata.org and make sure everything is there.')
 
